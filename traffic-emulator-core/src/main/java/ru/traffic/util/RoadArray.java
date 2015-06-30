be8a60fda198c6685443092d5b6393a73b65a70d
bryazginkos <bryazginkos@yahoo.com>
2015-06-30 22:57:55 +0600

    not generic

--- traffic-emulator-core/src/main/java/ru/traffic/util/RoadArray.java
+++ traffic-emulator-core/src/main/java/ru/traffic/util/RoadArray.java
@@ -1,33 +1,41 @@
 package ru.traffic.util;
 
+import ru.traffic.model.RoadPointInfo;
+
 import java.lang.reflect.Array;
 
 /**
  * Created by Константин on 30.06.2015.
  */
-public class RoadArray<T> {
+public class RoadArray {
 
     private final int length;
     private final int lanesNumber;
 
-    private final T[][] array;
+    private final RoadPointInfo[][] array;
 
     @SuppressWarnings("unchecked")
-    public RoadArray(int length, int lanesNumber, Class<T> tClass) {
+    public RoadArray(int length, int lanesNumber) {
         this.length = length;
         this.lanesNumber = lanesNumber;
-        array = (T[][]) Array.newInstance(tClass, length, lanesNumber);
+        array = (RoadPointInfo[][]) Array.newInstance(RoadPointInfo.class, length, lanesNumber);
     }
 
     //todo check params
-    public T get (int distance, int lane) {
+    public RoadPointInfo get (int distance, int lane) {
         return array[distance - 1][lane - 1];
     }
 
     //todo check params
-    public void put(int distance, int lane, T element) {
+    public void put(int distance, int lane, RoadPointInfo element) {
         array[distance - 1][lane - 1] = element;
     }
 
+    public int getLength() {
+        return length;
+    }
 
+    public int getLanesNumber() {
+        return lanesNumber;
+    }
 }
--- traffic-emulator-core/src/test/java/ru/traffic/util/RoadArrayTest.java
+++ traffic-emulator-core/src/test/java/ru/traffic/util/RoadArrayTest.java
@@ -3,6 +3,7 @@ package ru.traffic.util;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import ru.traffic.model.RoadPointInfo;
 
 /**
  * Created by Константин on 30.06.2015.
@@ -11,43 +12,43 @@ public class RoadArrayTest {
 
     private static final int LENGTH = 12;
     private static final int LANES_NUMBER = 4;
-    private RoadArray<String> roadArray;
+    private RoadArray roadArray;
+    private RoadPointInfo roadPointInfo;
 
     @Before
     public void initialization() {
-        roadArray = new RoadArray<String>(LENGTH, LANES_NUMBER, String.class);
+        roadArray = new RoadArray(LENGTH, LANES_NUMBER);
+        roadPointInfo = new RoadPointInfo(12, null);
     }
 
     @Test
     public void testAddElement() {
-        roadArray.put(3, 2, "hello");
+        roadArray.put(3, 2, roadPointInfo);
     }
 
     @Test
     public void testAddElementEnd() {
-        roadArray.put(LENGTH, 2, "hello");
+        roadArray.put(LENGTH, 2, roadPointInfo);
     }
 
     @Test
     public void testAddElementBegin() {
-        roadArray.put(1, 2, "hello");
+        roadArray.put(1, 2, roadPointInfo);
     }
 
     @Test
     public void testAddElementLeft() {
-        roadArray.put(3, 1, "hello");
+        roadArray.put(3, 1, roadPointInfo);
     }
 
     @Test
     public void testAddElementRight() {
-        roadArray.put(3, LANES_NUMBER, "hello");
+        roadArray.put(3, LANES_NUMBER, roadPointInfo);
     }
 
     @Test
     public void testGetElement() {
-        String str = "hello";
-        roadArray.put(3, 3, str);
-        String takeStr = roadArray.get(3, 3);
-        Assert.assertEquals(str, takeStr);
+        roadArray.put(3, 3, roadPointInfo);
+        Assert.assertEquals(roadPointInfo, roadArray.get(3, 3));
     }
 }
