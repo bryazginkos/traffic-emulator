f48171ffe63b675dc18102f85671d15c33d92e02
bryazginkos <bryazginkos@yahoo.com>
2015-07-09 19:31:31 +0600

    change lane (not safe yet)

--- traffic-emulator-core/src/main/java/ru/traffic/actors/CarActor.java
+++ traffic-emulator-core/src/main/java/ru/traffic/actors/CarActor.java
@@ -11,6 +11,7 @@ import ru.traffic.messages.manage.DeleteRoadPointMessage;
 import ru.traffic.messages.move.MoveMessage;
 import ru.traffic.model.Move;
 import ru.traffic.model.Position;
+import ru.traffic.model.RoadPointInfo;
 import ru.traffic.processing.InfoProcessor;
 import ru.traffic.util.RoadArray;
 
@@ -27,10 +28,15 @@ public class CarActor extends UntypedActor {
 
     private Position position;
 
+    private Position futurePosition;
+
+    private int freeSpace;
+
     public CarActor(ActorRef decisionActor, Car car, Position position) {
         this.decisionActor = decisionActor;
         this.car = car;
         this.position = position;
+        this.futurePosition = position;
     }
 
     @Override
@@ -43,22 +49,95 @@ public class CarActor extends UntypedActor {
     }
 
     private void nextTime(NextTimeMessage nextTimeMessage) {
-        log.info("start Thinking...");
-        RoadArray state = nextTimeMessage.getState();
+        position = futurePosition;
+        futurePosition = null;
+        log.info("start thinking...");
+        RoadArray<RoadPointInfo> state = nextTimeMessage.getState();
         try {
-            int freeSpace = InfoProcessor.freeFrontSpace(state, position, car.wishSpeed());
-            //todo use car class to get behaviour
-            Position futurePosition = new Position(position.getDistance() + freeSpace, position.getLane());
-            Move move = new Move(position, futurePosition);
-            position = futurePosition;
-            log.info("Send move");
-            decisionActor.tell(new MoveMessage(move), getSelf());
+            freeSpace = InfoProcessor.freeFrontSpace(state, position, car.wishSpeed());
+            if (freeSpace >= car.wishSpeed()) {
+                goStraight(car.wishSpeed());
+            } else if (freeSpace > 0) {
+                switch (car.slowFront()) {
+                    case SIDE: {
+                        wantChangeLane(car.wishSpeed(), state);
+                        break;
+                    }
+                    case SLOW: {
+                        goStraight(freeSpace);
+                        break;
+                    }
+                }
+            } else {
+                assert  freeSpace == 0;
+                switch (car.stopFront()) {
+                    case SIDE: {
+                        wantChangeLane(0, state);
+                        break;
+                    }
+                    case STOP: {
+                        goStraight(0);
+                        break;
+                    }
+                }
+            }
+
         } catch (OutOfViewException e) {
             log.info("send self-delete request");
             DeleteRoadPointMessage deleteRoadPointMessage = new DeleteRoadPointMessage(position);
             decisionActor.tell(deleteRoadPointMessage, getSelf());
             getContext().stop(getSelf());
         }
+
+    }
+
+    private void goStraight(int step) {
+        futurePosition = new Position(position.getDistance() + step, position.getLane());
+        Move move = new Move(position, futurePosition);
+        log.info("Send move (go straight " + step + " steps)");
+        decisionActor.tell(new MoveMessage(move), getSelf());
+    }
+
+    private void wantChangeLane(int speed, RoadArray<RoadPointInfo> state) {
+        if (position.getDistance() == 1) {
+            goStraight(freeSpace);
+        }
+        int laneAdd = chooseWishLane(state);
+        futurePosition = new Position(position.getDistance() + speed, position.getLane() + laneAdd);
+        Move move = new Move(position, futurePosition);
+        log.info("Send move (go straight " + speed + " steps, laneAdd=" + laneAdd + ")");
+        decisionActor.tell(new MoveMessage(move), getSelf());
+    }
+
+    private int chooseWishLane(RoadArray<RoadPointInfo> state) {
+        if (state.getLanesNumber() == 1) {
+            return 0;
+        }
+        if (position.getLane() == 1) {
+            return 1;
+        }
+        if (position.getLane() == state.getLanesNumber()) {
+            return  -1;
+        }
+        int i = 0;
+
+        while (i <= state.getLength() - position.getDistance()) {
+            int viewDistance = position.getDistance() + i;
+            RoadPointInfo right = state.get(viewDistance, position.getLane() + 1);
+            RoadPointInfo left =  state.get(viewDistance, position.getLane() - 1);
+            if (right == null && left != null) {
+                return 1;
+            }
+            if (right != null && left == null) {
+                return -1;
+            }
+            if (left != null && right != null) {
+                int speedDif = right.getSpeed() - left.getSpeed();
+                return speedDif/Math.abs(speedDif);
+            }
+            i++;
+        }
+        return -1;
 
     }
 }
