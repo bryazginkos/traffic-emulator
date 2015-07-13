bdbe3e3b2e354d155caadce58d8e938b9d455cd4
bryazginkos <bryazginkos@yahoo.com>
2015-07-13 15:38:51 +0600

    right colors

--- traffic-emulator-view/src/main/java/ru/traffic/view/util/RoadTransformer.java
+++ traffic-emulator-view/src/main/java/ru/traffic/view/util/RoadTransformer.java
@@ -6,8 +6,11 @@ import ru.traffic.model.RoadPointInfo;
 import ru.traffic.util.RoadArray;
 import ru.traffic.view.json.Road;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Created by Константин on 01.07.2015.
+ * Created by пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ on 01.07.2015.
  */
 @Component
 public class RoadTransformer {
@@ -41,11 +44,14 @@ public class RoadTransformer {
     }
 
     private String getColor(double politeness, double effrontery) {
+        double hue = politeness * 55/240;
+        double brightness = (69 + (210 - 69)*(1 -effrontery))/240;
+        Color c = Color.getHSBColor((float)hue, 1, (float)brightness);
         //calculated using linear function with move from PE plane to color plane
-        long red = Math.round(-politeness * 66 - effrontery * 184 + 253);
-        long green = Math.round(politeness * 66 - effrontery * 148 + 187);
-        long blue = Math.round(politeness * 5 - effrontery * 81 + 187);
-        return toHexString(red) + toHexString(green) + toHexString(blue);
+        long red = Math.round(c.getRed());
+        long green = Math.round(c.getGreen());
+        long blue = Math.round(c.getBlue());
+        return "#" + toHexString(red) + toHexString(green) + toHexString(blue);
     }
 
     private String toHexString(long arg) {
@@ -54,5 +60,34 @@ public class RoadTransformer {
             result = "0" + result;
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setSize(840,560);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().add(new Grid());
+        window.setVisible(true);
+    }
+
+    private static class Grid extends JComponent {
+        public void paint(Graphics g) {
+            double dp = 0.1;
+            double de = 0.1;
+            double p = 0;
+            double e = 0;
+            while (p <= 1) {
+                while (e <=1) {
+                    double hue = p * 55/240;
+                    double brightness = (69 + (210 - 69)*(1 -e))/240;
+                    Color c = Color.getHSBColor((float)hue, 1, (float)brightness);
+                    g.setColor(c);
+                    g.fillRect((int)Math.round(p* 100), (int)Math.round(e*100), 100, 100);
+                    e += de;
+                }
+                p += dp;
+                e = 0;
+            }
+        }
     }
 }
